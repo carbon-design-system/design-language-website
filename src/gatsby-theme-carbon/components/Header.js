ac370a77a8e482aaec776967dc1e7e2a8aec15e5
Alison Joseph <alisonejoseph@gmail.com>
2019-05-06 14:06:54 -0500

    feat: add navigation

---
+++ src/gatsby-theme-carbon/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'gatsby-theme-carbon/src/components/Header';
+
+const CustomSwitcher = () => (
+  <Header>
+    gatsby theme <span style={{ fontWeight: 200 }}>&nbsp;Carbon</span>
+  </Header>
+);
+
+export default CustomSwitcher;
