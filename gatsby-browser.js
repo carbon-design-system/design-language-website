aed50d91f311e3e4135cc3e69b8d7173392a0303
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-08-29 13:50:35 -0500

    fix: update plex resource, video and homepage tile resources

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
 import './src/styles/index.scss';
+
+// If production, import with "$font-prefix: /design/language/fonts"
+if (process.env.NODE_ENV === 'production') {
+  require('./src/styles/plex-variants-production.scss');
+} else {
+  // otherwise, import with "$font-prefix: /fonts"
+  require('./src/styles/plex-variants.scss');
+}
