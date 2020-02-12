43c120cd5df6a50eafc86511300649aa9080171b
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-12 09:55:16 -0600

    feat: add action dropdown menu

--- src/components/SvgLibraries/shared/SvgCard.js
+++ src/components/SvgLibraries/shared/SvgCard.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
-import React from 'react';
+import React, { useState } from 'react';
 import { pascal } from 'change-case';
+import { Menu, MenuItem } from './Menu';
 import {
   svgCard,
   svgCardInside,
@@ -8,21 +9,36 @@ import {
   triggerText,
 } from './SvgLibrary.module.scss';
 
-const SvgCard = ({ icon, ...rest }) => (
-  <li className={svgCard}>
-    <div className={svgCardInside}>
-      <span className={triggerText}>{icon.friendly_name}</span>
-      <div className={flexContainer}>
-        {icon.Component ? (
-          <icon.Component {...rest}>
-            <title>{icon.friendly_name}</title>
-          </icon.Component>
-        ) : (
-          <p>Error: no component found for {pascal(icon.friendly_name)}</p>
-        )}
+const SvgCard = ({ icon, ...rest }) => {
+  const [menuExpanded, setMenuExpanded] = useState(false);
+  const copyHandler = () => {
+    alert('You copied an icon');
+  };
+
+  const downloadHandler = () => {
+    alert('You downloaded an icon');
+  };
+
+  return (
+    <li className={svgCard}>
+      <div className={svgCardInside}>
+        <span className={triggerText}>{icon.friendly_name}</span>
+        <Menu expanded={menuExpanded} setExpanded={setMenuExpanded}>
+          <MenuItem onClick={copyHandler}>Copy component</MenuItem>
+          <MenuItem onClick={downloadHandler}>Download SVG</MenuItem>
+        </Menu>
+        <div className={flexContainer}>
+          {icon.Component ? (
+            <icon.Component {...rest}>
+              <title>{icon.friendly_name}</title>
+            </icon.Component>
+          ) : (
+            <p>Error: no component found for {pascal(icon.friendly_name)}</p>
+          )}
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default SvgCard;
