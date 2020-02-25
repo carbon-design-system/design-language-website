c2c21ad63eb2636fb87c6f0434bfacaf8c7385bc
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-24 19:40:29 -0600

    chore: add type

--- plugins/gatsby-theme-carbon-svgs/components/shared/ActionBar.js
+++ plugins/gatsby-theme-carbon-svgs/components/shared/ActionBar.js
@@ -30,6 +30,7 @@ const ActionBar = ({
   const handleDownload = () => {
     const a = document.body.appendChild(document.createElement('a'));
     a.download = `${name}.svg`;
+    a.type = `image/svg+xml`;
     a.href = withPrefix(`/${type}s/${name}.svg`);
     a.click();
     document.body.removeChild(a);
