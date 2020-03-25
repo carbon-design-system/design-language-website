6c095293a26c0ddca5f090cb8897596ddcb1545d
Greeshma Reddy Gaddam <greeshma.reddy.gaddam@ibm.com>
2020-03-25 14:30:52 -0400

    minicard

---
+++ src/components/MiniCard/MiniCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MiniCard.scss';
+
+
+
+
+
+
+const MiniCard = props => (
+    <a className='mini-card-link' href={props.link}>
+        <div className='mini-card'>
+            <p className='mini-card-label'> {props.labelName}</p>
+            <img className='mini-card-icon' alt={props.iconImage} src={props.iconImage}/>
+        </div>
+    </a>
+);
+
+
+export default MiniCard;
+
+MiniCard.propTypes = {
+    link: PropTypes.string,
+    iconImage: PropTypes.string,
+    labelName: PropTypes.string,
+    description: PropTypes.string
+
+};
+
