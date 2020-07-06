14f15cfb8e9e98d1612608408b692c967234e04e
sadekbazaraa <33162280+sadekbazaraa@users.noreply.github.com>
2020-07-06 13:38:49 -0400

    Add animation pages and illustration usage tabs v3 (#548)
    Co-authored-by: SADEK BAZARAA <sadekbazaraa1@SDKSonos.local>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andrea N. Cardona <andreancardona@gmail.com>
    Co-authored-by: Josefina Mancilla <josefinanoemi4@gmail.com>
    Co-authored-by: Vince Picone <Vincent.Patrick.Picone@ibm.com>
    Co-authored-by: SADEK BAZARAA <sadekbazaraa1@SDKSonos.lan>
    Co-authored-by: Vince Picone <vpicone@gmail.com>

---
+++ src/components/LottiePlayer/LottiePlayer.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import Lottie from 'react-lottie';
+
+const LottiePlayer = props => {
+  const [data, setData] = useState(null);
+
+  const path = props.src
+    .then(module => {
+      setData(module.default);
+    })
+    .catch(console.log);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: data,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return <Lottie width={'100%'} height={'100%'} options={defaultOptions} />;
+};
+
+export default LottiePlayer;
