3f8ffd1a29eb4c7cdcd0ce15088208637061f444
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-07-16 19:10:28 -0500

    chore: update eslint config

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,5 @@
-/* eslint-disable react/no-danger */
+import React from 'react';
+
 export const onRenderBody = ({ setPostBodyComponents }) => {
   if (process.env.NODE_ENV !== `production`) {
     return null;
