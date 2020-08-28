9d8d42942482e9e401e42816f94eaa1a3a14a2a3
Filip Frincu <Filip.Frincu@ibm.com>
2020-08-28 12:55:24 +0300

    feat: add FeedbackDialog component

---
+++ src/gatsby-theme-carbon/components/FeedbackDialog/FeedbackDialog.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ThemeFeedbackDialog from 'gatsby-theme-carbon/src/components/FeedbackDialog/FeedbackDialog';
+import * as axios from 'axios';
+import { ToastNotification } from 'carbon-components-react';
+
+const REQUEST_KEY = 'b64cb88a-fab4-4e4f-8ab3-07574522a125';
+const SURVEY_ID = '5806027';
+
+const successMessage = {
+  title: 'Feedback sent',
+  subtitle: 'Thank you for submiting your feedback.',
+  kind: 'success',
+};
+const failMesage = {
+  title: 'Feedback not sent',
+  subtitle: 'Please contact the website administrator.',
+  kind: 'error',
+};
+
+let timeoutInstance;
+
+const FeedbackDialog = ({ props }) => {
+  const [notification, setNotification] = useState({});
+
+  const showNotification = (notificationOpt) => {
+    clearTimeout(timeoutInstance);
+    setNotification(notificationOpt);
+    timeoutInstance = setTimeout(() => {
+      setNotification({});
+    }, 4500);
+  };
+
+  const onSubmit = (feedbackData) => {
+    const config = {
+      method: 'post',
+      params: {
+        ...feedbackData,
+        surveyId: SURVEY_ID,
+      },
+      url: 'https://b3fa4946.us-south.apigw.appdomain.cloud/feedback/submit',
+      headers: {
+        'X-IBM-Client-Id': REQUEST_KEY,
+      },
+    };
+
+    axios(config)
+      .then((res) => {
+        const { success } = res.data;
+
+        if (success) {
+          showNotification(successMessage);
+        } else {
+          showNotification(failMesage);
+        }
+      })
+      .catch(() => {
+        showNotification(failMesage);
+      });
+  };
+
+  return (
+    <>
+      {Object.keys(notification).length !== 0 &&
+        notification.constructor === Object && (
+          <div style={{ position: 'fixed', top: 45, right: 0 }}>
+            <ToastNotification
+              iconDescription="Close"
+              title={notification.title}
+              subtitle={<span>{notification.subtitle}</span>}
+              kind={notification.kind}
+              caption=""
+            />
+          </div>
+        )}
+
+      {
+        // eslint-disable-next-line
+        <ThemeFeedbackDialog {...props} onSubmit={onSubmit} />
+      }
+    </>
+  );
+};
+
+FeedbackDialog.propTypes = {
+  props: PropTypes.object,
+};
+
+export default FeedbackDialog;
