683e76feaf6f121e20a31366cc36220438b2f17a
Leonardo da Costa <leocosta@ibm.com>
2025-10-27 17:07:25 -0300

    feat: shadowing Minicard component

---
+++ src/gatsby-theme-carbon/components/MiniCard/MiniCard.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import {
+  ArrowRight,
+  Calendar,
+  Download,
+  Email,
+  Launch,
+  LogoGithub,
+} from '@carbon/icons-react';
+import { settings } from 'carbon-components';
+import cx from 'classnames';
+
+import * as styles from './MiniCard.module.scss';
+import { Column } from 'gatsby-theme-carbon/src/components/Grid';
+
+const { prefix } = settings;
+
+const getIcon = ({ actionIcon }) => {
+  switch (actionIcon) {
+    case 'arrowRight':
+      return <ArrowRight size={20} aria-label="Open resource" />;
+    case 'download':
+      return <Download size={20} aria-label="Download" />;
+    case 'email':
+      return <Email size={20} aria-label="Email" />;
+    case 'calendar':
+      return <Calendar size={20} aria-label="Calendar" />;
+    case 'github':
+      return <LogoGithub size={20} aria-label="Github" />;
+    default:
+      return <Launch size={20} aria-label="Open resource" />;
+  }
+};
+
+const MiniCard = ({
+  children,
+  href,
+  title,
+  actionIcon,
+  className,
+  linkProps,
+  ...rest
+}) => {
+  const cardContent = (
+    <div className={cx(className, styles.card)}>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>{title}</div>
+        {children === undefined ? (
+          <div className={styles.icon}>{getIcon({ actionIcon })}</div>
+        ) : (
+          <div className={styles.image}>{children}</div>
+        )}
+      </div>
+    </div>
+  );
+
+  let isLink;
+  if (href !== undefined) {
+    isLink = href.charAt(0) === '/';
+  }
+
+  const cardContainer = isLink ? (
+    <Link to={href} className={`${prefix}--tile--clickable`} {...linkProps}>
+      {cardContent}
+    </Link>
+  ) : (
+    <a href={href} className={`${prefix}--tile--clickable`} {...linkProps}>
+      {cardContent}
+    </a>
+  );
+
+  return (
+    <Column colMd={4} colLg={4} noGutterSm {...rest}>
+      {cardContainer}
+    </Column>
+  );
+};
+
+export default MiniCard;
